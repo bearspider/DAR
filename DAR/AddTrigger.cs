db0e2980ba8d27bf3cb0346121a002fc48b1b75e
Jared <jrhaddix@gmail.com>
2018-09-26 08:49:28 -0500

    Not even sure

--- DAR/AddTrigger.cs
+++ DAR/AddTrigger.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LiteDB;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,12 +13,21 @@ namespace DAR
 {
     public partial class AddTrigger : Form
     {
-        public AddTrigger()
+        public AddTrigger(String database)
         {
             InitializeComponent();
             (tabControl1.TabPages[2] as TabPage).Enabled = false;
             (tabControl1.TabPages[3] as TabPage).Enabled = false;
             comboBoxTimerType.SelectedIndex = 1;
+            using (var db = new LiteDatabase(database))
+            {
+                LiteCollection<CharacterProfile> characterProfiles = db.GetCollection<CharacterProfile>("profiles");
+                var col = db.GetCollection<CharacterProfile>("profiles");
+                foreach (var doc in col.FindAll())
+                {
+                    listviewCharacters.Items.Add(doc.ProfileName);
+                }
+            }
         }
     }
 }

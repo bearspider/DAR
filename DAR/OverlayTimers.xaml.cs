ecad351d0b93c77ce639dc8bd9c0cc5ea4d3904c
Unknown <jrhaddix@gmail.com>
2018-10-03 15:41:35 -0500

    Messing around with timerbars

--- DAR/OverlayTimers.xaml.cs
+++ DAR/OverlayTimers.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Timers;
@@ -23,25 +25,35 @@ namespace DAR
     /// </summary>
     public partial class OverlayTimers : Window
     {
-        public ObservableCollection<ProgressBar> timerBars = new ObservableCollection<ProgressBar>();
-        private ProgressBar progress = new ProgressBar();
-        System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+        public ObservableCollection<TriggerTimer> timerBars = new ObservableCollection<TriggerTimer>();
         public OverlayTimers()
         {
             InitializeComponent();
-            progress.Minimum = 1;
-            progress.Maximum = 10;
-            dispatcherTimer.Tick += DispatcherTimer_Tick;
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
-            dispatcherTimer.Start();
-            timerBars.Add(progress);
+            var listener = OcPropertyChangedListener.Create(timerBars);
+            listener.PropertyChanged += Listener_PropertyChanged;
+            TriggerTimer newTimer = new TriggerTimer();
+            newTimer.SetProgress(1, 60);
+            newTimer.SetTimer("This is a new timer", 60, true);
+            newTimer.StartTimer();
+            timerBars.Add(newTimer);
+            TriggerTimer newTimer1 = new TriggerTimer();
+            newTimer1.SetProgress(1, 60);
+            newTimer1.SetTimer("This is a new timer1", 60, true);
+            newTimer1.StartTimer();
+            timerBars.Add(newTimer1);
             listviewTimers.ItemsSource = timerBars;
         }
 
-        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        private void Listener_PropertyChanged(object sender, PropertyChangedEventArgs e)
         {
-            progress.Value++;
+            if(((TriggerTimer)sender).ProgressValue == ((TriggerTimer)sender).Maximum)
+            {
+                timerBars.Remove((TriggerTimer)sender);
+            }
+
+
         }
+
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if(e.ChangedButton == MouseButton.Left)
@@ -56,6 +68,113 @@ namespace DAR
             {
                 this.Opacity = .4;
             }
+        }
+
+    }
+    public class OcPropertyChangedListener<T> : INotifyPropertyChanged where T : INotifyPropertyChanged
+    {
+        private readonly ObservableCollection<T> _collection;
+        private readonly string _propertyName;
+        private readonly Dictionary<T, int> _items = new Dictionary<T, int>(new ObjectIdentityComparer());
+        public OcPropertyChangedListener(ObservableCollection<T> collection, string propertyName = "")
+        {
+            _collection = collection;
+            _propertyName = propertyName ?? "";
+            AddRange(collection);
+            CollectionChangedEventManager.AddHandler(collection, CollectionChanged);
+        }
+
+        private void CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    AddRange(e.NewItems.Cast<T>());
+                    break;
+                case NotifyCollectionChangedAction.Remove:
+                    RemoveRange(e.OldItems.Cast<T>());
+                    break;
+                case NotifyCollectionChangedAction.Replace:
+                    AddRange(e.NewItems.Cast<T>());
+                    RemoveRange(e.OldItems.Cast<T>());
+                    break;
+                case NotifyCollectionChangedAction.Move:
+                    break;
+                case NotifyCollectionChangedAction.Reset:
+                    Reset();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+        }
+
+        private void AddRange(IEnumerable<T> newItems)
+        {
+            foreach (T item in newItems)
+            {
+                if (_items.ContainsKey(item))
+                {
+                    _items[item]++;
+                }
+                else
+                {
+                    _items.Add(item, 1);
+                    PropertyChangedEventManager.AddHandler(item, ChildPropertyChanged, _propertyName);
+                }
+            }
+        }
+
+        private void RemoveRange(IEnumerable<T> oldItems)
+        {
+            foreach (T item in oldItems)
+            {
+                _items[item]--;
+                if (_items[item] == 0)
+                {
+                    _items.Remove(item);
+                    PropertyChangedEventManager.RemoveHandler(item, ChildPropertyChanged, _propertyName);
+                }
+            }
+        }
+
+        private void Reset()
+        {
+            foreach (T item in _items.Keys.ToList())
+            {
+                PropertyChangedEventManager.RemoveHandler(item, ChildPropertyChanged, _propertyName);
+                _items.Remove(item);
+            }
+            AddRange(_collection);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void ChildPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(sender, e);
+        }
+
+        private class ObjectIdentityComparer : IEqualityComparer<T>
+        {
+            public bool Equals(T x, T y)
+            {
+                return object.ReferenceEquals(x, y);
+            }
+            public int GetHashCode(T obj)
+            {
+                return System.Runtime.CompilerServices.RuntimeHelpers.GetHashCode(obj);
+            }
+        }
+    }
+
+    public static class OcPropertyChangedListener
+    {
+        public static OcPropertyChangedListener<T> Create<T>(ObservableCollection<T> collection, string propertyName = "") where T : INotifyPropertyChanged
+        {
+            return new OcPropertyChangedListener<T>(collection, propertyName);
         }
     }
 }
---
+++ DAR/TriggerTimer.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace DAR
+{
+    public class TriggerTimer : INotifyPropertyChanged
+    {
+        public DispatcherTimer dispatcherTimer;
+        public String timerDescription;
+        public int timerDuration;
+        public ProgressBar progress;
+        public Boolean direction; //false is count down, true is count up
+        public double progressValue;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public TriggerTimer ()
+        {
+            dispatcherTimer = new DispatcherTimer()
+            {
+                Interval = new TimeSpan(0, 0, timerDuration)//hours,minutes,seconds
+            };
+            timerDescription = "New Timer";
+            timerDuration = 0;
+            direction = false;
+            progress = new ProgressBar()
+            {
+                Minimum = 0,
+                Maximum = 1
+            };
+        }
+        public double Minimum
+        {
+            get { return progress.Minimum; }
+        }
+        public double Maximum
+        {
+            get { return progress.Maximum; }
+        }
+        public double ProgressValue
+        {
+            get { return progressValue; }
+            set { progressValue = value; }
+        }
+        public ProgressBar Progress
+        {
+            get { return progress; }
+            set { progress = value; }
+        }
+        public DispatcherTimer DispatcherTimer
+        {
+            get { return dispatcherTimer; }
+            set { dispatcherTimer = value; }
+        }
+        public String TimerDescription
+        {
+            get { return timerDescription; }
+            set { timerDescription = value; }
+        }
+        public int TimerDuration
+        {
+            get { return timerDuration; }
+            set { timerDuration = value; }
+        }
+        public double GetProgress()
+        {
+            return progress.Value;
+        }
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            if(direction)
+            {
+                progress.Value++;                
+            }
+            else
+            {
+                progress.Value--;
+            }
+            progressValue = progress.Value;
+            NotifyPropertyChanged("Value");
+        }
+        public void SetTimer(String description, int duration, Boolean count)
+        {
+            timerDescription = description;
+            timerDuration = duration;
+            direction = count;
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+        }
+        public void StartTimer()
+        {
+            dispatcherTimer.Start();            
+        }
+        public void StopTimer()
+        {
+            dispatcherTimer.Stop();
+        }
+        public void SetProgress(int minimum, int maximum)
+        {
+            progress.Minimum = minimum;
+            progress.Maximum = maximum;
+        }
+
+        void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+    }
+}
